346e8638f66ba1724b0bca25d28fbb6ad97ab60e
RaufunNazin <raufun.nazin13@gmail.com>
2025-05-18 14:20:09 +0600

    get specific onu details

--- helper.py
+++ helper.py
@@ -171,8 +171,7 @@ async def get_olt_information(target_ip, community_string, port, version, retrie
         else:
             varBindList = varBinds
     else:
-        # Use nextCmd (async iterator)
-        objects = await next_cmd(
+        objects = walk_cmd(
             snmp_engine,
             community,
             transport,
